fcce86c32a71dbe4818435628fc706e212470488
Eduard Hovhannisyan <66397209+eduard0497@users.noreply.github.com>
2022-04-25 04:02:18 -0700

    other overflow fixes

--- components/UserRelated/AllUserPosted/DisplayPostedHousesForUser.js
+++ components/UserRelated/AllUserPosted/DisplayPostedHousesForUser.js
@@ -9,44 +9,50 @@ function DisplayPostedHousesForUser({ data, title }) {
     return (
       <div className={styles.user_dashboard_posted_houses_container}>
         <h1>{title}</h1>
-        <table className={styles.user_dashboard_posted_houses_container_table}>
-          <thead>
-            <tr>
-              <th>Listing ID</th>
-              <th>Title</th>
-              <th>Price</th>
-              {data[0].frequency ? <th>Frequency</th> : null}
-              <th>Location</th>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Listing Type</th>
-              <th>Date Posted</th>
-              {data[0].expires ? <th>Expires</th> : null}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => {
-              return (
-                <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.title}</td>
-                  <td>${item.price}</td>
-                  {item.frequency ? <td>{item.frequency} </td> : null}
-                  <td>
-                    {item.city}, {item.state} {item.zip}
-                  </td>
-                  <td>{item.name}</td>
-                  <td>{item.phone}</td>
-                  <td>{item.is_special ? "SPECIAL" : "REGULAR"}</td>
-                  <td>{new Date(item.timestamp).toLocaleDateString()}</td>
-                  {item.expires ? (
-                    <td>{new Date(item.expires).toLocaleDateString()} </td>
-                  ) : null}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div
+          className={styles.user_dashboard_posted_houses_container_table_outer}
+        >
+          <table
+            className={styles.user_dashboard_posted_houses_container_table}
+          >
+            <thead>
+              <tr>
+                <th>Listing ID</th>
+                <th>Title</th>
+                <th>Price</th>
+                {data[0].frequency ? <th>Frequency</th> : null}
+                <th>Location</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Listing Type</th>
+                <th>Date Posted</th>
+                {data[0].expires ? <th>Expires</th> : null}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => {
+                return (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>${item.price}</td>
+                    {item.frequency ? <td>{item.frequency} </td> : null}
+                    <td>
+                      {item.city}, {item.state} {item.zip}
+                    </td>
+                    <td>{item.name}</td>
+                    <td>{item.phone}</td>
+                    <td>{item.is_special ? "SPECIAL" : "REGULAR"}</td>
+                    <td>{new Date(item.timestamp).toLocaleDateString()}</td>
+                    {item.expires ? (
+                      <td>{new Date(item.expires).toLocaleDateString()} </td>
+                    ) : null}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
