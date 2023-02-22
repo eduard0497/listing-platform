4f22715ccdb4dc37b31395bc0a40185107eeb983
Eduard Hovhannisyan <66397209+eduard0497@users.noreply.github.com>
2023-02-22 01:48:53 -0800

    final touch

--- components/UsefulFunctions/prices.js
+++ components/UsefulFunctions/prices.js
@@ -2,47 +2,47 @@ export const allPrices = [
   {
     id: 1,
     name: "Video Advertisement",
-    price: "120",
+    price: "90",
     expires: "30",
-    stripe_link: "https://buy.stripe.com/test_4gw15l5nJ1FL7ks3ce",
-    location_image_link: "/locationsForAdTypes/VideoAds.png"
+    stripe_link: "https://buy.stripe.com/fZeeYM9xfbpq6aIfZ5",
+    location_image_link: "/locationsForAdTypes/VideoAds.png",
   },
   {
     id: 2,
     name: "Top Banner Advertisement",
-    price: "80",
+    price: "70",
     expires: "90",
-    stripe_link: "https://buy.stripe.com/test_00g7tJ9DZfwB8ow4gj",
-    location_image_link: "/locationsForAdTypes/BannerAds.png"
+    stripe_link: "https://buy.stripe.com/eVacQEcJr512fLi3ci",
+    location_image_link: "/locationsForAdTypes/BannerAds.png",
   },
   {
     id: 3,
     name: "Side Small Image Advertisement",
-    price: "60",
+    price: "50",
     expires: "30",
-    stripe_link: "https://buy.stripe.com/test_14k3dtaI31FL9sA8wA",
-    location_image_link: "/locationsForAdTypes/SideAds.png"
+    stripe_link: "https://buy.stripe.com/00gcQE4cV0KM0QodQV",
+    location_image_link: "/locationsForAdTypes/SideAds.png",
   },
   {
     id: 4,
     name: "Running Sentence Advertisement",
-    price: "40",
+    price: "25",
     expires: "10",
-    stripe_link: "https://buy.stripe.com/test_7sI8xN17t1FL208cMR",
-    location_image_link: "/locationsForAdTypes/RunningAds.png"
+    stripe_link: "https://buy.stripe.com/9AQaIweRz0KMeHefZ2",
+    location_image_link: "/locationsForAdTypes/RunningAds.png",
   },
   {
     id: 5,
     name: "Special Listing (Will be visible on Homepage & with regular listings)",
-    price: "30",
+    price: "15",
     expires: "30",
-    stripe_link: "https://buy.stripe.com/test_6oEg0feYj705eMUdQW",
+    stripe_link: "https://buy.stripe.com/4gw8Ao24NfFGgPmaEH",
   },
   {
     id: 6,
     name: "Regular Listing (Will NOT be visible on Homepage)",
-    price: "10",
+    price: "5",
     expires: "30",
-    stripe_link: "https://buy.stripe.com/test_aEUdS7cQb849dIQbIP",
+    stripe_link: "https://buy.stripe.com/00g9EsaBjbpq2Yw7su",
   },
 ];
